4cecfc3a94e74fcf4e9ad8bd4a95b84f4b69189a
Vitaliy Podoba <vitaliypodoba@gmail.com>
2013-01-25 11:50:40 +0200

    add missing notes

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.5.2'
+version = '1.5.3'
 
 tests_require = ['zope.testing']
 
