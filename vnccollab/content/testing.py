9c18674251da6cef3fdb744936d64594c676f82e
José Dinuncio <jose.dinuncio@gmail.com>
2013-11-13 16:44:46 -0430

    Improves testing infrastructure.

--- vnccollab/content/testing.py
+++ vnccollab/content/testing.py
@@ -1,10 +1,10 @@
+from plone.testing import z2
+
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
-
-from zope.configuration import xmlconfig
 
 
 class AddonContent(PloneSandboxLayer):
@@ -13,15 +13,21 @@ class AddonContent(PloneSandboxLayer):
 
     def setUpZope(self, app, configurationContext):
         # Load ZCML
-        import raptus.autocompletewidget
-        self.loadZCML(package=raptus.autocompletewidget)
+        dependencies = [
+            'raptus.autocompletewidget',
+            'collective.customizablePersonalizeForm',
+            'vnccollab.content',
+        ]
 
-        import vnccollab.content
-        xmlconfig.file('configure.zcml',
-                       vnccollab.content,
-                       context=configurationContext)
+        for package in dependencies:
+            module = __import__(package, fromlist=[''])
+            self.loadZCML(package=module)
+
+        z2.installProduct(app, 'collective.customizablePersonalizeForm')
+        z2.installProduct(app, 'vnccollab.content')
 
     def setUpPloneSite(self, portal):
+        applyProfile(portal, 'collective.customizablePersonalizeForm:default')
         applyProfile(portal, 'vnccollab.content:default')
 
 
@@ -31,4 +37,4 @@ VNCCOLLAB_CONTENT_INTEGRATION_TESTING = IntegrationTesting(
     name='AddonContent:Integration')
 VNCCOLLAB_CONTENT_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(VNCCOLLAB_CONTENT_FIXTURE,),
-    name='VNCThemeContent:Functional')
+    name='AddonContent:Functional')
