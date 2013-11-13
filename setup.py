6fa042e73dd20166838e9e74d0ca1eb9835d5ec0
José Dinuncio <jose.dinuncio@gmail.com>
2013-11-13 15:17:37 -0430

    Adds collective.customizablePersonalizeForm.

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(name='vnccollab.content',
       install_requires=[
           'setuptools',
           'raptus.autocompletewidget',
+          'collective.customizablePersonalizeForm',
       ],
       extras_require={'test': ['plone.app.testing']},
       entry_points="""
