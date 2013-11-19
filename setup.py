e99309617218417db49804918c884e1a5562fd3e
José Dinuncio <jose.dinuncio@gmail.com>
2013-11-19 11:39:28 -0430

    Adds dependencies.

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setup(name='vnccollab.content',
       install_requires=[
           'setuptools',
           'five.grok',
+          'plone.api',
           'simplejson',
           'raptus.autocompletewidget',
           'collective.customizablePersonalizeForm',
