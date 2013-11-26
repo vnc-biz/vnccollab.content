89161ebb88249fb9399054c79145c0b1d263bb35
César González <gonzalec@gmail.com>
2013-11-25 21:34:37 -0430

    Adds follow js code. Adds ajax to new author page. Adds vanilla style.

--- vnccollab/content/browser/js/main.js
+++ vnccollab/content/browser/js/main.js
@@ -1,63 +1,128 @@
-//
-// Deferred Portlets
-//
-function initDeferredPortlets() {
-  function deferredUrlInfo(elem) {
-    var $elem = jq(elem);
-    var manager = $elem.attr('portlet-manager');
-    var name    = $elem.attr('portlet-name');
-    var key     = $elem.attr('portlet-key');
+// module definition
+var vnc_collab_content = (function () {
 
-    if (!manager || ! name || !key) {
-      return '';
-    }
+  // Private components
 
-    return ({
-      'url': window.location.origin + window.location.pathname + '/portlet_deferred_render',
-      'data': {
-        'manager': manager,
-        'name': name,
-        'key': key
+  //
+  // Following Controls
+  //
+
+  function initFollowingControls() {
+    // attach hover actions
+    jq('a.followLink,a.unfollowLink').mouseover(function(event){
+      var link = $(event.target);
+      link.data('orig_label', link.text()).text(link.attr('title'));
+    }).mouseout(function(event){
+      var link = $(event.target);
+      if (link.data('orig_label')) {
+        link.text(link.data('orig_label'));
+      }
+    });
+
+    // attach click handlers to Follow/Unfollow buttons
+    jq('a.followLink,a.unfollowLink').click(function(event){
+       event.preventDefault();
+       event.stopPropagation();
+       var link = $(event.target),
+        path = link.is('.followLink') ? '@@follow_user' : '@@unfollow_user';
+
+      jq.ajax({
+        'url': portal_url + '/' + path,
+        'type': 'POST',
+        'dataType': 'json',
+        'data': {'user1': '', 'user2': link.data('userid')},
+        'success': function(data, status, xhr){
+          link.text(data['label']).attr('title', data['title'])
+            .data('orig_label', '');
+          if (link.is('.followLink')) {
+            link.removeClass('followLink').addClass('unfollowLink');
+          } else {
+            link.removeClass('unfollowLink').addClass('followLink');
+          }
+          return false;
+        },
+        'error': function(){
+          alert('Sorry, something went wrong on the server. Please, try a ' +
+            'bit later.');
+          return false;
+        }
+      });
+
+      return false;
+    });
+  }
+
+  //
+  // User profile tabs Handler
+  //
+
+  function initUserProfileTabHandler() {
+    jq('#userprofile-tabmenu').on('click', 'a', function(event) {
+      event.preventDefault();
+
+      var page =  '.' + jq(this).data('pageTab');
+      var href = jq(this).attr('href');
+
+      // select/unselect tab menu option
+      jq('#userprofile-tabmenu a').removeClass('selected');
+      jq(this).addClass('selected');
+
+      // show/hide tab panel
+      jq('.formPanel').hide();
+      jq('#userprofile-tabcontents').find(page).show();
+
+      // if dynamic content then load it
+      if ( href  != '#') {
+        // start spinner
+        jq('#userprofile-spinner').show();
+
+        var content = loadTab(href);
+        jq('#userprofile-tabcontents').find(page).empty().append(content);
+
+      }
+
+      // load content tab by ajax
+      function loadTab(url) {
+          var content = '';
+
+          jq.ajax({
+            type: 'GET',
+            url: url,
+            dataType: 'html',
+            async: false,
+            cache: false,
+            success: function( data ){
+              var $content = jq(data).find('#content');
+
+              jq('#userprofile-spinner').hide();
+
+              // remove unneccesary content
+              $content.find('.backLink').remove();
+              $content.find('h1').remove();
+
+              content = $content.html();
+            },
+            error: function(){
+              jq('#userprofile-spinner').hide();
+              content = '';
+            }
+          });
+          return content;
       }
     });
   }
 
-  function updatePortlet(elem){
-    // Returns a funciton to update the portlet represented by elem DOM
-    var fn = function(data) {
-      var $elem = jq(elem),
-          $data = jq(data);
-  
-      // We want to be sure we got the portlet and not an error page
-      if ($data.hasClass('portlet-deferred')) {
-        $data.find('.portletBody').slimScroll({'height': '240px'});
-        $elem.replaceWith($data);
-        attachPortletButtons();
-      } else {
-        $elem.find('.portletBodyWrapper').empty();
-      }
-    }
-    return fn;
-  }
-
-  function deferredRender() {
-    // Starts the deferred render of the portlet,
-    // if it has enough info
-    var urlInfo = deferredUrlInfo(this);
-    if (!urlInfo) {
-      return;
-    }
-
-    var url = urlInfo.url;
-    var data = urlInfo.data;
-    jq.get(url, data, updatePortlet(this));
-  }
-
-  var deferredPortlets = jq('.portlet-deferred');
-  deferredPortlets.each(deferredRender);
-}
+  // public interface
+  return {
+        initFollowingControls: initFollowingControls,
+        initUserProfileTabHandler: initUserProfileTabHandler
+  };
+}) ();
 
 
+// run on load
 jq(function() {
-     initDeferredPortlets();
-   });
+  var me = vnc_collab_content;
+  me.initFollowingControls();
+  me.initUserProfileTabHandler();
+});
